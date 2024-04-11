e6cb6cd92e2b2e1e21fc49b6878ae04fcbdc14ba
cursosenai <166341518+cursosenai@users.noreply.github.com>
2024-04-10 21:07:22 -0300

    Update SENSOR_UMIDADE.ino

--- SENSOR_UMIDADE.ino
+++ SENSOR_UMIDADE.ino
@@ -1,18 +1,35 @@
+#include <DHT.h>
 #include <SoftwareSerial.h>
 
+
 SoftwareSerial bluetooth(2,3);
+
+#define HT A0
+#define TipoDHT DHT11
+
+DHT dht(HT, TipoDHT);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("teste de DHT");
   bluetooth.begin(9600);
+  dht.begin();
 }
 
 void loop() {
-  Serial.print("Percentual: ");
+  float u = dht.readHumidity();
+  if(isnan(u)){
+    Serial.println("erro com DHT");
+    return;
+
+  }
+  Serial.print("Umidade: ");
   Serial.print(u);
   Serial.println("%");
-  Serial.println("Dados Enviado");
+
+  Serial.println("Dados Enviadis");
   bluetooth.print(u);
-  delay(1000);
+
+  delay(300);
+
 }
